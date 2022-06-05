e60e9df4fc7c03ae6c3026c5953bfa310614075d
yulia633 <ignat633yulia@yandex.ru>
2022-06-05 20:39:19 +0300

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,25 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use PDO;
+use App\DB;
+
+/**
+ * Example user model
+ */
 class User
 {
-    //
+    /**
+     * Get all the users as an associative array
+     *
+     * @return array
+     */
+    public static function getAll()
+    {
+        $sql = "SELECT * FROM users";
+        $stm = DB::getInstance()->prepare($sql);
+        $stm->execute();
+
+        return $stm->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
